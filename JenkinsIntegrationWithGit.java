781ce28cca8f233243b614a086952c09013fbc95
Mharo <m.haroon.emran2016@gmail.com>
2022-11-19 11:39:18 -0500

    Video8 Hello world

---
+++ JenkinsIntegrationWithGit.java
@@ -0,0 +1,14 @@
+
+public class JenkinsIntegrationWithGit {
+	public static void main(String[] args) {
+		
+		System.out.println("Hey Now look At my Code");
+		
+		int i;
+		for (i=1;i<=10;i++)
+		{
+			System.out.println("I am the ROck");
+		}
+	}
+
+}
